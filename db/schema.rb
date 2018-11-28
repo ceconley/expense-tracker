9e16593c28d7c7f76eab74746dee3b6060b49a0e
ceconley4 <ceconley4@gmail.com>
2018-11-28 13:34:05 -0500

    added user id to expenses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_143019) do
+ActiveRecord::Schema.define(version: 2018_11_28_182936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2018_11_26_143019) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +45,5 @@ ActiveRecord::Schema.define(version: 2018_11_26_143019) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "expenses", "users"
 end
