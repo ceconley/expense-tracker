5190f19e2fcf1231e38dad57f04b47b5caa922a4
ceconley <ceconley4@gmail.com>
2019-01-29 15:12:39 -0500

    added method to change date format

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,8 @@
 class Expense < ApplicationRecord
   belongs_to :user
+
+  def created_at_display
+    self[:created_at].strftime("%m/%d/%Y")
+  end
+
 end
--- app/serializers/expense_serializer.rb
+++ app/serializers/expense_serializer.rb
@@ -1,3 +1,3 @@
 class ExpenseSerializer < ActiveModel::Serializer
-  attributes :id, :source, :amount, :category, :description, :created_at
+  attributes :id, :source, :amount, :category, :description, :created_at_display
 end
